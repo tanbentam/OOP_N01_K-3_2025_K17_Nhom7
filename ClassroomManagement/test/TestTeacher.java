e9d4484fde43edbd52857eb09eb393d542220db5
Chu Thành Tân <96729383+tanbentam@users.noreply.github.com>
2025-05-11 18:26:23 +0700

    Update TestTeacher.java

--- ClassroomManagement/test/TestTeacher.java
+++ ClassroomManagement/test/TestTeacher.java
@@ -4,23 +4,23 @@ import static org.junit.Assert.*;
 public class TestTeacher {
     @Test
     public void testTeacherCreation() {
-        Teacher teacher = new Teacher("T01", "Thầy An", "Toán");
+        Teacher teacher = new Teacher("T01", "Thầy Tráng", "CNTT");
         assertEquals("T01", teacher.getId());
-        assertEquals("Thầy An", teacher.getName());
-        assertEquals("Toán", teacher.getSubject());
+        assertEquals("Thầy Tráng", teacher.getName());
+        assertEquals("CNTT", teacher.getSubject());
     }
 
     @Test
     public void testSetName() {
-        Teacher teacher = new Teacher("T01", "Thầy An", "Toán");
+        Teacher teacher = new Teacher("T01", "Thầy Tráng", "CNTT");
         teacher.setName("Cô Lan");
         assertEquals("Cô Lan", teacher.getName());
     }
 
     @Test
     public void testSetSubject() {
-        Teacher teacher = new Teacher("T01", "Thầy An", "Toán");
-        teacher.setSubject("Văn");
-        assertEquals("Văn", teacher.getSubject());
+        Teacher teacher = new Teacher("T01", "Thầy Tráng", "CNTT");
+        teacher.setSubject("Kinh tế");
+        assertEquals("Kinh tế", teacher.getSubject());
     }
 }
