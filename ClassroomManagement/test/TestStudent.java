997a9b4a5c91d8dd2314901b3355cea38411435e
Chu Thành Tân <96729383+tanbentam@users.noreply.github.com>
2025-05-11 18:24:08 +0700

    Update TestStudent.java

--- ClassroomManagement/test/TestStudent.java
+++ ClassroomManagement/test/TestStudent.java
@@ -4,22 +4,22 @@ import static org.junit.Assert.*;
 public class TestStudent {
     @Test
     public void testStudentCreation() {
-        Student student = new Student("S01", "Nam", 16);
+        Student student = new Student("S01", "Tân", 20);
         assertEquals("S01", student.getId());
-        assertEquals("Nam", student.getName());
-        assertEquals(16, student.getAge());
+        assertEquals("Tân", student.getName());
+        assertEquals(20, student.getAge());
     }
 
     @Test
     public void testSetName() {
-        Student student = new Student("S01", "Nam", 16);
+        Student student = new Student("S01", "Tân", 20);
         student.setName("Lan");
         assertEquals("Lan", student.getName());
     }
 
     @Test
     public void testSetAge() {
-        Student student = new Student("S01", "Nam", 16);
+        Student student = new Student("S01", "Tân", 20);
         student.setAge(17);
         assertEquals(17, student.getAge());
     }
