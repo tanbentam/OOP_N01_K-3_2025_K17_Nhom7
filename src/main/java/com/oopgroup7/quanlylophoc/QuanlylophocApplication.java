b9cdb2754c171719889d8d5aaecb21e8ac28b2c9
Chu Thanh Tan <chuthanhtan1995@gmail.com>
2025-06-20 17:52:45 +0700

    xay dung lai cau truc thu muc theo mo hinh MVC

---
+++ src/main/java/com/oopgroup7/quanlylophoc/QuanlylophocApplication.java
@@ -0,0 +1,13 @@
+package com.oopgroup7.quanlylophoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlylophocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlylophocApplication.class, args);
+	}
+
+}
